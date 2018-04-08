4400edcf6e497f0c4575b081abd1b09b0a45edea
AleArioli <alessandro.arioli@italianway.it>
2018-04-08 15:44:09 +0200

    dynamic host added

--- public/socket-client.js
+++ public/socket-client.js
@@ -1,4 +1,5 @@
-var socket = io.connect('ws://127.0.0.1:3000');
+var host = location.origin.replace(/^http/, 'ws')
+var socket = io.connect(host);
 var nickname, rightLeft;
 
 function ScrollToBottom() {
